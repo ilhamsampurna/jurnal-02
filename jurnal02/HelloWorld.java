444774c10cce828463b10f2df6cf19820dbef40c
ilhamsampurna <ilhamsampurna56@gmail.com>
2020-09-17 16:43:30 +0700

    HelloWorld.java

---
+++ jurnal02/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama  : Ilham Sampurna");
+		System.out.println("NIM   : 6706202066");
+		System.out.println("Kelas : D3IF-44-02");
+	}
+}
